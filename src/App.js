3d1ff12852dd5201bc7a0a11d90ab19ba5135f37
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-07 22:40:43 -0400

    needs contact section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import './App.css';
 import { Link, Events, animateScroll as scroll, scroller } from 'react-scroll';
 
+import ttcimg from './assets/tic-tac-toe.png'
+import tasklist from './assets/tasklist.png'
+
 export class App extends Component {
 
   constructor(props) {
@@ -50,7 +53,7 @@ export class App extends Component {
         </ul>
       </div>
       <div className="name-container">
-        <h1 className="name">Michael Beaudry</h1>
+        <h1 className="name">MICHAEL BEAUDRY</h1>
         <h5 className="positions">Web Developer / Marketing</h5>
         {/* <div name="test1" className="scroll-to-about"></div> */}
       </div>
@@ -64,6 +67,28 @@ export class App extends Component {
       <div className="projects-container-main" name="test3">
         <h3 className="projects-title">Projects</h3>
         <div className="projects-container">
+          <div className="box">
+            <div className="imgbox">
+              <img className="projects-img" src={ttcimg}alt="p1"/>
+            </div>
+            <a href="https://youthful-beaver-86231a.netlify.com/">
+              <div className="caption">
+                <h5 className="caption-h">Classic Tic-Tac-Toe</h5>
+                <p className="caption-p">React/Javascript/HTML/SCSS</p>
+              </div>
+            </a>
+          </div>
+          <div className="box">
+            <div className="imgbox">
+              <img className="projects-img" src={tasklist} alt="p1"/>
+            </div>
+            <a href="https://cranky-lamarr-f3677e.netlify.app/">
+              <div className="caption">
+                <h5 className="caption-h">Task List</h5>
+                <p className="caption-p">Javascript/HTML/CSS</p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
