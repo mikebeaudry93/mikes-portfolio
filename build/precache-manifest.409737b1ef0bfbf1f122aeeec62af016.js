7f2e7d96d73e808fcf6fe8f313ef3cac0722ccfc
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-28 19:34:32 -0400

    added build folder

---
+++ build/precache-manifest.409737b1ef0bfbf1f122aeeec62af016.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3274b4a06dcffa7dcf678ca3f2b61c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfacfa81ed927b268362",
+    "url": "/static/css/main.be5da2a9.chunk.css"
+  },
+  {
+    "revision": "59d476b3115420c0b2d6",
+    "url": "/static/js/2.8ced601d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ced601d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfacfa81ed927b268362",
+    "url": "/static/js/main.dfee41c1.chunk.js"
+  },
+  {
+    "revision": "f2f6de7fa72db7815659",
+    "url": "/static/js/runtime-main.f8fe8526.js"
+  },
+  {
+    "revision": "a505f9b24c9f1a68343f2521fa97b3a7",
+    "url": "/static/media/car.a505f9b2.jpg"
+  },
+  {
+    "revision": "294e017759949289c7e6401260a8b78c",
+    "url": "/static/media/expense-tracker.294e0177.png"
+  },
+  {
+    "revision": "24b62548b48af3e019a3c82df54758a8",
+    "url": "/static/media/github2.24b62548.jpg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "7658e5e563315777886535da05f90835",
+    "url": "/static/media/quizpic.7658e5e5.png"
+  },
+  {
+    "revision": "d71af0b2a733f0f8f43dedad641a211b",
+    "url": "/static/media/react.d71af0b2.png"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/static/media/sass.b446ca5a.svg"
+  },
+  {
+    "revision": "021935babd20c435f7d7f82527bf6f14",
+    "url": "/static/media/tasklist.021935ba.png"
+  },
+  {
+    "revision": "25f4bd6d14d8761770025a93978180ff",
+    "url": "/static/media/tic-tac-toe.25f4bd6d.png"
+  },
+  {
+    "revision": "7ad1fab9e7d39e01443f7cbc9542005d",
+    "url": "/static/media/weather-app1.7ad1fab9.png"
+  }
+]);
